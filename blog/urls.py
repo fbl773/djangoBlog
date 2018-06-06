b6e27470c89549eabed58df148889c98e1a1aad8
fbl773 <fbl773@mail.usask.ca>
2018-06-06 16:54:44 -0600

    finished tutorial.... here we go bois

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
-	re_path(r'^post/(?P<pk>\d+)/$',views.entry_detail,name='entry_detail'),
-	path('',views.post_list,name='post_list'),
+	re_path(r'^entry/(?P<pk>\d+)/$',views.entry_detail,name='entry_detail'),
+	re_path(r'^entry/new/$', views.entry_new, name="entry_new"),
+	path('',views.entry_list,name='entry_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Entry
+from .forms import EntryForm
 
-def post_list(request):	
+def entry_list(request):	
 	entries = Entry.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'blog/post_list.html',{'entries':entries})
+	return render(request, 'blog/entry_list.html',{'entries':entries})
 
 def entry_detail(request,pk):
 	entry = get_object_or_404(Entry, pk=pk)
 	return render(request, 'blog/entry_detail.html',{'entry':entry})
 
-
+def entry_new(request):
+	form=EntryForm()
+	return render(request, 'blog/entry_edit.html', {'form':form})
 
 # Create your views here.
