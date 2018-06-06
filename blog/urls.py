0359c53b6619db62f3be9e0e5ff886fd659090a6
fbl773 <fbl773@mail.usask.ca>
2018-06-06 16:32:25 -0600

    added some views and css that doesn't render

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
+	re_path(r'^post/(?P<pk>\d+)/$',views.entry_detail,name='entry_detail'),
 	path('',views.post_list,name='post_list'),
 ]
